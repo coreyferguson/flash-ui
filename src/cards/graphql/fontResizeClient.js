17a5577be5171712b0195942941de4817bd446fc
Corey Ferguson <corey.t.ferguson@gmail.com>
2019-10-04 19:55:12 -0700

    consider mobile devices when calculating max width

--- src/cards/graphql/fontResizeClient.js
+++ src/cards/graphql/fontResizeClient.js
@@ -9,7 +9,7 @@ const timeout = ms => new Promise(resolve => setTimeout(resolve, ms));
 export async function autoSizeText(cardId, side, element, hasImage, defaultFontSize) {
   const setSize = value => element.setAttribute('style', `font-size: ${value}%`);
   const removeSize = () => element.removeAttribute('style');
-  const maxAllowedWidth = 500;
+  const maxAllowedWidth = Math.min(element.offsetWidth, element.parentElement.offsetWidth-10);
   const maxAllowedHeight = !hasImage ? 250 : 100;
 
   let node = element;
