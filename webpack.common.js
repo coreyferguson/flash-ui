0e2609a15aa2d888e2ad008aef9658679142af51
Corey Ferguson <corey.t.ferguson@gmail.com>
2020-04-30 18:24:04 -0700

    Removing sass loader from webpack.

--- webpack.common.js
+++ webpack.common.js
@@ -32,15 +32,6 @@ module.exports = {
         loader: 'babel-loader'
       }
     }, {
-      test: /\.scss$/,
-      use: [{
-        loader: 'style-loader' // creates style nodes from JS strings
-      }, {
-        loader: 'css-loader' // translates CSS into CommonJS
-      }, {
-        loader: 'sass-loader' // compiles Sass to CSS
-      }]
-    }, {
       test: /\.(png|svg|jpg|gif)$/,
       use: [ 'file-loader' ]
     }]
